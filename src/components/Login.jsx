a5401f987c3f0777b120cca5727844dc65694996
egon-isnoise <egonisnoise@proton.me>
2022-09-10 14:06:07 +0200

    redux reducer 1rst

---
+++ src/components/Login.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useDispatch } from "react-redux";
+import { login } from "../features/user";
+
+function Login() {
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+       <button onClick={() => {
+          dispatch(login({name:"Pedro", age: 20, email:"pedro@gmail.com"}));
+        }}
+       >
+        Log in
+      </button>
+    </div>
+  )
+}
+
+export default Login;
